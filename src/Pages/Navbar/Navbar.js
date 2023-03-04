282c999fd37a77ff1cc7224d3ec5277afd47b985
kobipro <Boatengkwabenaakuamoah@gmail.com>
2023-03-04 17:17:17 +0000

    created the checkout component with functionality

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -9,55 +9,54 @@ function Navbar() {
   // eslint-disable-next-line 
   const [{ basket, user }, dispatch] = useStateValue();
   return (
-    <>
-    <div className="header">
-      
+    <><>
+      <div className="header">
+
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          alt="navbar "
-        />
-      
+          alt="navbar " />
 
-      <div className="header__search">
-        <input className="header__searchInput" type="text" />
-        <SearchIcon className="header__searchIcon" />
-      </div>
 
-      <div className="header__nav">
-        
+        <div className="header__search">
+          <input className="header__searchInput" type="text" />
+          <SearchIcon className="header__searchIcon" />
+        </div>
+
+        <div className="header__nav">
+
           <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
             <Link to="/signin"><span className="header__optionLineTwo">Sign In</span></Link>
           </div>
-        
 
-        
+
+
           <div className="header__option">
             <span className="header__optionLineOne">Returns</span>
             <span className="header__optionLineTwo">& Orders</span>
           </div>
-        
-        
 
-        <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineTwo">Prime</span>
+
+
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+
+
+          <Link to="/checkout">
+            <ShoppingBasketIcon />
+          </Link>
+          <span className="header__optionLineTwo header__basketCount">
+            {basket?.length}
+          </span>
         </div>
 
-        
-          <div className="header__optionBasket">
-            <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">
-              {basket?.length}
-            </span>
-          </div>
-        
       </div>
-    </div>
-    <Outlet />
-     </>
-  );
+    </><Outlet />
+    </>
+     );
 }
 
 export default Navbar;
